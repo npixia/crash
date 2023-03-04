b7e338822c3906efe1662ae72662f73571d17ef8
npixia <npixia@gmail.com>
2023-03-04 13:43:06 -0500

    Add center rooms to world gen

--- src/spaceship_world_generator.lua
+++ src/spaceship_world_generator.lua
@@ -52,38 +52,57 @@ function SpaceShip:generateMap(universe_seed, map, width, height, x, y, z, spawn
 
     local rooms = {room_2_a, room_2_b, center_room}
 
-
     for _=0,rng:random(2,5) do
-        self.addSplitRoom(map, rng, offset, half_ch, rooms)
+        if rng:random() < 0.5 then
+            self.addSplitRoom(map, rng, offset, half_ch, rooms)
+        else
+            self.addCenterRoom(map, rng, offset, rooms)
+        end
     end
 end
 
-function SpaceShip.addSplitRoom(map, rng, offset, half_ch, rooms)
-    S = Point(rng:random(7,14), rng:random(3,14))
-    local min_overlap = -half_ch
-    local room_offset = rng:random(min_overlap+1, 10)
-    print(f'min_overlap={min_overlap}, room_offest={room_offset}')
-    local room_x = 0
-    local done = false
-    local start_x = 100
+function SpaceShip.addCenterRoom(map, rng, offset, rooms)
+    local half_h = rng:random(2, 20)
+    local S = Point(rng:random(4, 8), half_h * 2 + 1)
+    local room_y = -half_h
+    local room_x = SpaceShip.findBackOfShipIntersection(rooms, room_y, S.x, S.y)
+    if room_x < 0 then
+        print('no intersect found')
+    else
+        local room = ShipRoom(room_x, room_y, S.x, S.y)
+        room:place(map, offset)
+        table.insert(rooms, room)
+    end
+    
+end
 
+function SpaceShip.findBackOfShipIntersection(rooms, room_y, room_width, room_height)
+    local start_x = 100
     for i=0,100 do
-        room_x = start_x - i
-        if done then break end
-        local room = ShipRoom(room_x, half_ch + room_offset, S.x, S.y)
+        local room_x = start_x - i
+        local room = ShipRoom(room_x, room_y, room_width, room_height)
         for _, r in ipairs(rooms) do
             -- TODO: Need to check that interior intersect
             if room:intersects(r) then
-                done = true
-                break
+                return room_x - 1
             end
         end
     end
+    return -1 -- not found
+end
 
-    if done == false then
+function SpaceShip.addSplitRoom(map, rng, offset, half_ch, rooms)
+    local S = Point(rng:random(7,14), rng:random(3,14))
+    local min_overlap = -half_ch
+    local room_offset = rng:random(min_overlap+1, 10)
+    print(f'min_overlap={min_overlap}, room_offest={room_offset}')
+    local room_y_1 = half_ch + room_offset
+    local room_x = SpaceShip.findBackOfShipIntersection(rooms, room_y_1, S.x, S.y)
+
+    if room_x < 0 then
         print('No intersect found')
     else
-        local split_room_a = ShipRoom(room_x, half_ch + room_offset, S.x, S.y)
+        local split_room_a = ShipRoom(room_x, room_y_1, S.x, S.y)
         local split_room_b = ShipRoom(room_x, -(half_ch+S.y-1) - room_offset, S.x, S.y)
         split_room_a:place(map, offset)
         split_room_b:place(map, offset)
