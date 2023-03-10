66ab5b49b6f734a7e1f13bf49c8f40fd864d5913
npixia <npixia@gmail.com>
2023-03-09 21:17:24 -0500

    Interacting with a door takes a turn

--- autoload/doors.lua
+++ autoload/doors.lua
@@ -6,9 +6,11 @@ local map = game.world.currentMap
 local function doorInteraction(closed_id, open_id)
     game.tiles.addInteraction(closed_id, 'open', function(actor, x, y)
         toggleTile(map(), x, y, actor, getTile(closed_id), getTile(open_id))
+        wait(100)
     end)
     game.tiles.addInteraction(open_id, 'close', function(actor, x, y)
         toggleTile(map(), x, y, actor, getTile(open_id), getTile(closed_id))
+        wait(100)
     end)
 
 end
