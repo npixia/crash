e0b9edd5c26b3c7ae8457c7755a4a8e92ee92042
npixia <npixia@gmail.com>
2023-03-10 16:57:06 -0500

    Add death condition and welcome message

--- src/init.lua
+++ src/init.lua
@@ -1,5 +1,34 @@
 requirep 'crash:spaceship_world_generator'
 
+local welcome_message = [[
+You wake up and remember that space ship had a power failure and has crash-landed on an unknown planet.
+You must descend into your ship's hull and manually override the [color=yellow]backup power[/color]. Then return here to the [color=yellow]navigation console[/color] to escape.
+Beware, the crash landing has attracted attention of the [color=green]locals[/color].
+
+=== IMPORTANT KEYBINDS ===
+
+Press [color=green]?[/color] for help and keybind list
+
+[color=orange]View[/color]:
+Press [color=green]alt +[/color] or [color=green]alt -[/color] to scale the text & tiles
+[color=green]Z[/color]: Zoom in/out, Center View
+
+[color=orange]Gameplay[/color]:
+Move using [color=green]WASD[/color]
+[color=green]SPACE OR LMB[/color]: Use Item
+[color=green]I[/color]: Inventory
+[color=green]L[/color]: Look
+[color=green]E OR RMB[/color]: Interact
+[color=green]G[/color]: Pick Up (Some items are auto-pickup)
+[color=green]P[/color]: Equipment
+[color=green]/[/color]: Open message log ([color=green]ESC[/color]) to close
+
+[color=orange]Menus/Other[/color]:
+[color=green]ESC[/color] or [color=green]Q[/color] to exit menus, dialogs, and screens
+Press [color=green]ENTER[/color] or [color=green]SPACE[/color] to select things
+]]
+
+
 -- Config
 game.config.setZoom(true)
 game.config.setAutomoveWaitTime(120)
@@ -13,13 +42,18 @@ game.signal.subscribe('/world/load/complete', function()
         game.world.teleport(128, 128, 0, 0, -1, '')
     end
     game.world.data().first_load_complete = true
+
+    game.config.setKeybinding('z', 'Zoom', function()
+        game.render.setCamera(game.actors.getPlayer():getXY())
+        game.config.setZoom(not game.config.getZoom())
+    end)
+
 end)
 
 -- Called after the first render tick has completed after the world has loaded
 game.signal.subscribe('/world/load/render_complete', function()
 
     if not game.world.data().has_seen_welcome_message then
-        local welcome_message = requirep 'welcome_message'
         local keep_reading = 0
         -- User must select "Play" in order to exit the loop
         while keep_reading == 0 or keep_reading == 1 do
@@ -27,6 +61,12 @@ game.signal.subscribe('/world/load/render_complete', function()
             keep_reading = game.gui.menuSelect('Ready to play?', {'Show Welcome Message', 'Play'})
         end
         game.world.data().has_seen_welcome_message = true
+    end
+
+    if game.world.data().player_dead then
+        game.gui.messageBox('You Died!')
+        wait(100)
+        game.exit()
     end
 end)
 
@@ -38,6 +78,9 @@ end)
 
 -- Called when the player dies
 game.signal.subscribe('/player/death', function()
+    game.world.data().player_dead = true
+    game.save()
+
     local loop = true
     while loop do
         game.gui.messageBox('You Died!', 'You Died!')
